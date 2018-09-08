3dcd390e10fd4070fbf6a098b2a7cb94bba962cf
Madh93 <mhdez@protonmail.com>
2018-09-08 13:18:33 +0100

    Update URL

--- nombramiento-maestros/start.rb
+++ nombramiento-maestros/start.rb
@@ -2,8 +2,8 @@
 
 require 'net/http'
 
-url = "http://www.gobcan.es/educacion/Nombramientos/Documentos/NDPTF" \
-      "#{Time.now.strftime('%d-%m-%y')}.PDF"
+url = "http://www.gobiernodecanarias.org/educacion/Nombramientos/Documentos/" \
+      "NDPTF#{Time.now.strftime('%d-%m-%y')}.PDF"
 
 uri = URI.parse(url)
 result = Net::HTTP.start(uri.host, uri.port) { |http| http.get(uri.path) }
